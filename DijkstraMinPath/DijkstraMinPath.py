befb81f319d648bb482c7d9837e0b9471340b03a
Rafael Rossi <rarossi@technip.com>
2016-08-31 09:22:03 +0200

    working Dijkstra w figure (slow)

--- DijkstraMinPath/DijkstraMinPath.py
+++ DijkstraMinPath/DijkstraMinPath.py
@@ -1,130 +1,132 @@
 global MINIMUM
 MINIMUM = 999999
 
+
 class Edge():
-# defines and edge, with a length d
-# and connected to two vertexes, v1 and v2
-# v1 and v2 are just the indexes of a
-# list with all vertexes
+    """Defines and edge, with a length d and connected to two vertexes, v1 and v2
+    v1 and v2 are just the indexes of a list with all vertexes
+    """
     def __init__(self, v1, v2, d):
         self.v1 = v1
         self.v2 = v2
-        self.d  =  d
+        self.d = d
+
 
 class Vertex():
-# defines a vertex and some other properties
-# for the Dijkstra algorithm
-# .distance : distance from the current vertex to the
-#             initial vertex, which is user defined
-# .unvisited: boolean flag for Dijkstra algorithm
-# .nearest  : index of the nearest vertex to the current one.
-#             this is used to show the full path with minimum distance
-#             from the initical vertex to the target vertex
-# .edges    : list with all edges connected to this vertex
-# .otherVert: list of the vertex on the other side of the connected
-#             edges.
-#
+    """defines a vertex and some other properties for the Dijkstra algorithm
+    .distance : distance from the current vertex to the
+                initial vertex, which is user defined
+    .unvisited: boolean flag for Dijkstra algorithm
+    .nearest  : index of the nearest vertex to the current one.
+                this is used to show the full path with minimum distance
+                from the initical vertex to the target vertex
+    .edges    : list with all edges connected to this vertex
+    .otherVert: list of the vertex on the other side of the connected
+                edges.
+    """
     def __init__(self, distance, unvisited, nearest):
-        self.distance   = distance
-        self.unvisited  = unvisited
-        self.nearest    = nearest
-        self.edges      = []
-        self.otherVert  = []
+        self.distance = distance
+        self.unvisited = unvisited
+        self.nearest = nearest
+        self.edges = []
+        self.otherVert = []
 
-# sub-function of the dijkstraGraph
-# returns the index of the next vertex to
-# be worked by the algorithm, which is the
-# unvisited vertex with the overall minimum
-# distance from the initial vertex
+
 def nextVertex(vertexes):
-    min=MINIMUM
+    """sub-function of the dijkstraGraph. returns the index of the next vertex to
+    be worked by the algorithm, which is the unvisited vertex with the overall minimum
+    distance from the initial vertex
+    """
+    min = MINIMUM
     for i, v in enumerate(vertexes):
-        if v.unvisited and v.distance<min:
-            min=v.distance
-            imin=i
+        if v.unvisited and v.distance < min:
+            min = v.distance
+            imin = i
     return imin
 
-# sub-function of the dijkstraGraph
-# for a given current vertex, goes through
-# all its still unvisited connected vertexes,
-# and check if the path through the current vertex
-# is smaller than the path actually stored
-# -> does not return anything since updates are done
-# directly in the list of vertexes
+
 def updateDistances(vertexes, edges, icurVertex):
+    """sub-function of the dijkstraGraph for a given current vertex, goes through
+    all its still unvisited connected vertexes, and check if the path through the current vertex
+    is smaller than the path actually stored
+    -> does not return anything since updates are done directly in the list of vertexes
+    """
     for i, v in enumerate(vertexes[icurVertex].otherVert):
         if vertexes[v].unvisited:
-            alt= ( vertexes[icurVertex].distance +
-                   edges[vertexes[icurVertex].edges[i]].d )
+            alt = (vertexes[icurVertex].distance +
+                   edges[vertexes[icurVertex].edges[i]].d)
             if alt < vertexes[v].distance:
                 vertexes[v].distance = alt
-                vertexes[v].nearest  = icurVertex
+                vertexes[v].nearest = icurVertex
 
-# main Dijkstra algorithm for min path finding
-# for a given starting vertex, calculates the minimum
-# distances to all the other vertexes.
-# these minimum distances are saved in the vertexes
-# themselves by accessing vertex[i].distance
-# -> does not return anything since updates are done
-# directly in the list of vertexes
-def dijkstraGraph(vertexes, edges, initialVertex):
-    #initial setup
+
+def dijkstraGraph(vertexes, edges, initialVertex, verbose=False):
+    """main Dijkstra algorithm for min path finding for a given starting vertex, calculates the
+    minimum distances to all the other vertexes. these minimum distances are saved in the vertexes
+    themselves by accessing vertex[i].distance
+    -> does not return anything since updates are done directly in the list of vertexes
+    """
+    # initial setup
     vertexes[initialVertex].distance = 0
-    while sum([_.unvisited for _ in vertexes]): 
+    left = 1
+    while left:
+        if verbose: print(left)
         icur = nextVertex(vertexes)
-        vertexes[icur].unvisited=False
-        #check all neighbors and update distances
+        vertexes[icur].unvisited = False
+        # check all neighbors and update distances
         updateDistances(vertexes, edges, icur)
+        left = sum([_.unvisited for _ in vertexes])
 
 
-#MAIN
+if __name__ == '__main__':
+    # MAIN
 
-# define the lengths of the edges and at
-# which vertex they are connected
-# !!!
-# NOTE see attached example.pdf for the graph of this example
-# !!!
-NumVertexes = 11
-MyEdges=[Edge(0,1,1),   Edge(0,2,999), Edge(0,3,555), Edge(1,4,1),
-         Edge(1,5,444), Edge(2,5,1),   Edge(3,5,1),   Edge(3,6,1),
-         Edge(4,7,1),   Edge(4,5,99),  Edge(5,8,100), Edge(5,6,200),
-         Edge(6,9,1),   Edge(7,10,1),  Edge(7,8,55),  Edge(8,10,1),
-         Edge(8,9,1),   Edge(9,10,333)]
+    # define the lengths of the edges and at
+    # which vertex they are connected
+    # !!!
+    # NOTE see attached example.pdf for the graph of this example
+    # !!!
+    NumVertexes = 11
+    MyEdges = [Edge(0, 1, 1), Edge(0, 2, 999), Edge(0, 3, 555), Edge(1, 4, 1),
+               Edge(1, 5, 444), Edge(2, 5, 1), Edge(3, 5, 1), Edge(3, 6, 1),
+               Edge(4, 7, 1), Edge(4, 5, 99), Edge(5, 8, 100), Edge(5, 6, 200),
+               Edge(6, 9, 1), Edge(7, 10, 1), Edge(7, 8, 55), Edge(8, 10, 1),
+               Edge(8, 9, 1), Edge(9, 10, 333)]
 
-# initialise the list with the vertexes, with a
-# high initial value for minimum distance (MINIMUM),
-# all marked as unvisited and nearest vertexes marked as -1
-MyVertexes = [Vertex(MINIMUM, True, -1) for _ in range(NumVertexes)]
+    # initialise the list with the vertexes, with a
+    # high initial value for minimum distance (MINIMUM),
+    # all marked as unvisited and nearest vertexes marked as -1
+    MyVertexes = [Vertex(MINIMUM, True, 0) for _ in range(NumVertexes)]
 
-# loops through all the edges and mark at which
-# vertexes they are connected to.
-# also marks in each vertex what's the vertex
-# on the other side of each edge
-# for example:
-#   V4---E8---V2---E3---V7
-# vertex[2].edges=[3,8]
-# vertex[2].otherVert=[7,4]
-for i in range(len(MyEdges)):
-    MyVertexes[MyEdges[i].v1].edges.append(i)
-    MyVertexes[MyEdges[i].v1].otherVert.append(MyEdges[i].v2)
-    MyVertexes[MyEdges[i].v2].edges.append(i)
-    MyVertexes[MyEdges[i].v2].otherVert.append(MyEdges[i].v1)
+    # loops through all the edges and mark at which
+    # vertexes they are connected to.
+    # also marks in each vertex what's the vertex
+    # on the other side of each edge
+    # for example:
+    #   V4---E8---V2---E3---V7
+    # vertex[2].edges=[3,8]
+    # vertex[2].otherVert=[7,4]
+    for i in range(len(MyEdges)):
+        MyVertexes[MyEdges[i].v1].edges.append(i)
+        MyVertexes[MyEdges[i].v1].otherVert.append(MyEdges[i].v2)
+        MyVertexes[MyEdges[i].v2].edges.append(i)
+        MyVertexes[MyEdges[i].v2].otherVert.append(MyEdges[i].v1)
 
-#calculate minimum distances to each vertex
-InitialVertex = 0
-dijkstraGraph(MyVertexes, MyEdges, InitialVertex)
+    # calculate minimum distances to each vertex
+    InitialVertex = 0
+    dijkstraGraph(MyVertexes, MyEdges, InitialVertex, verbose=True)
 
-#get the minimum path from InitialVertex to TargetVertex
-TargetVertex = 2
-n=TargetVertex
-MinPath=[n]
-while n:
-    n=MyVertexes[n].nearest
-    MinPath.append(n)
-MinPath.reverse()
+    # get the minimum path from InitialVertex to TargetVertex
+    TargetVertex = 2
+    n = TargetVertex
+    MinPath = [n]
+    while n:
+        n = MyVertexes[n].nearest
+        MinPath.append(n)
+    MinPath.reverse()
 
-#show results
-print "Minimum distance from vertex", InitialVertex,"to vertex",
-print TargetVertex, "is", MyVertexes[TargetVertex].distance
-print "The path for minimum distance is through vertexes:"
-print MinPath
+    # show results
+    print("Minimum distance from vertex", InitialVertex, "to vertex ", end='')
+    print(TargetVertex, "is", MyVertexes[TargetVertex].distance)
+    print("The path for minimum distance is through vertexes:")
+    print(MinPath)
---
+++ DijkstraMinPath/min_path_figure.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 30 14:31:04 2016
+
+@author: rarossi
+"""
+
+from matplotlib import pylab
+import numpy as np
+import DijkstraMinPath
+from DijkstraMinPath import Edge, Vertex
+
+
+def ascii_print(m):
+    for rw in m:
+        for p in rw:
+            print('#' if p > 1 else '.', end='')
+        print('')
+
+
+def vid(i, j, width):
+    """vertex id"""
+    return width*i+j
+
+
+def div(vid, width):
+    """inverse of vid"""
+    return int(vid/width), vid % width
+
+
+def calc_num_vertex(h, w):
+    """height, width --> number of vertexes in a rectangular grid"""
+    return 2 * (h-1) * (w-1) + h + w - 2
+
+
+def save_fig(fig, min_path, width, name='minpath.bmp'):
+    for p in min_path:
+        i, j = div(p, width)
+        fig[i][j] = np.array([255, 0, 0], dtype=np.uint8)
+    pylab.imshow(fig)
+
+
+# fig = pylab.imread('beta.bmp')
+# fig = pylab.imread('beta_3x4.bmp')
+fig = pylab.imread('beta_75x100.bmp')
+height, width = len(fig), len(fig[0])
+
+matrix = np.ones((height, width), dtype=int)
+
+# Converts the image into a matrix
+# Anything darker than medium grey will be a "wall"
+# The rest is "walkable" area
+for i in range(height):
+    for j in range(width):
+        matrix[i][j] = int(99) if fig[i][j][0] < 128 else 1
+
+# Set-up all the edges
+edges = list()
+for i in range(height):
+    for j in range(width):
+        if i < height-1:
+            edges.append(Edge(vid(i, j, width), vid(i+1, j, width), max(matrix[i][j],
+                                                                        matrix[i+1][j])))
+        if j < width-1:
+            edges.append(Edge(vid(i, j, width), vid(i, j+1, width), max(matrix[i][j],
+                                                                        matrix[i][j+1])))
+
+num_vertex = height*width
+vertexes = [Vertex(DijkstraMinPath.MINIMUM, True, 0) for _ in range(num_vertex)]
+
+for i in range(len(edges)):
+        vertexes[edges[i].v1].edges.append(i)
+        vertexes[edges[i].v1].otherVert.append(edges[i].v2)
+        vertexes[edges[i].v2].edges.append(i)
+        vertexes[edges[i].v2].otherVert.append(edges[i].v1)
+
+# calculate minimum distances to each vertex
+init_vertex = vid(0, 0, width)
+# init_vertex = vid(1, 0, width)
+DijkstraMinPath.dijkstraGraph(vertexes, edges, init_vertex, verbose=True)
+
+# get the minimum path from InitialVertex to TargetVertex
+target_vertex = vid(height-1, width-1, width)
+# target_vertex = vid(0, 2, width)
+n = target_vertex
+MinPath = [n]
+i, iter_limit = 0, 999
+while n and i < iter_limit:
+    n = vertexes[n].nearest
+    MinPath.append(n)
+    i += 1
+if i == iter_limit:
+    print('Warning: reached maximum number of iteractions.')
+MinPath.reverse()
+
+# show results
+print("Minimum distance from vertex", init_vertex, "to vertex ", end='')
+print(target_vertex, "is", vertexes[target_vertex].distance)
+print("The path for minimum distance is through vertexes:")
+print(MinPath)
+
+save_fig(fig, MinPath, width)
