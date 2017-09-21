23b75dfd6b8fa7ee731d9f33b82794211cd82014
Rafael Rossi <RaRossi@technip.com>
2017-09-21 16:27:39 +0200

    Results visualiser working beautifuly :)
    Need to:
    
     - document code properly
    
     - work out how to deploy it

--- Qt/ResultsVisualiser/results_loader.py
+++ Qt/ResultsVisualiser/results_loader.py
@@ -1,42 +1,52 @@
 import pandas
+import numpy as np
 
 class ResultsLoader():
-    def __init__(self, fname='results.txt'):
-        self.load_results(fname)
-    
-    def load_results(self, fname):
+    def __init__(self):
+        self.isAvailable = False
+
+    def load(self, fname):
         self.df = pandas.read_table(fname, sep='\t')
+        self.calc_cdf()
+        self.isAvailable = True
     
     def get_vars(self):
         return self.df.columns[3:]
         
     def get_hs_list(self):
-        hs = list(set(self.df['WaveHs']))
+        hs = list(set(self.df.WaveHs))
         hs.sort()
         return ['%.2f' % x for x in hs]
     
     def get_wd_list(self):
-        wd = list(set(self.df['WaveDirection']))
+        wd = list(set(self.df.WaveDirection))
         wd.sort()
         return ['%.1f' % x for x in wd]
         
     def get_tp_list(self, hs_list):
-        print(hs_list)
         if not hs_list:
             return []
-        if isinstance(hs_list[0], (int, float)):
+        if isinstance(hs_list[0], str):
             hs_list = [float(x) for x in hs_list]
         tp = set()
         for hs in hs_list:
-            tp.update(self.df[self.df['WaveHs'] == hs]['WaveTp'])
+            tp.update(self.df[self.df.WaveHs == hs]['WaveTp'])
         tp = list(tp)
         tp.sort()
         return ['%.2f' % x for x in tp]
     
     def get_sample(self, var, hs, tp, wd):
-        return df[(df.WaveHs == hs) & (df.WaveTp == tp) & 
-                  (df.WaveDirection == wd)][var].sort_values()
+        return self.df[(self.df.WaveHs == hs) & (self.df.WaveTp == tp) & 
+                       (self.df.WaveDirection == wd)][var].sort_values()
     
+    def calc_cdf(self):
+        seeds = len(self.df[(self.df.WaveHs == self.df.WaveHs[0]) & 
+                            (self.df.WaveTp == self.df.WaveTp[0]) & 
+                            (self.df.WaveDirection == self.df.WaveDirection[0])])
+        self.seeds = seeds
+        self.cdf = np.linspace(0.5/seeds, (seeds-0.5)/seeds, seeds)
+        self.llcdf = -np.log(-np.log(self.cdf))
+
     def check_data(self):
         """TBC
         perform some sanity checks, like number of seeds, check that
--- Qt/ResultsVisualiser/results_visualiser.py
+++ Qt/ResultsVisualiser/results_visualiser.py
@@ -6,69 +6,134 @@ Created on Wed Sep 20 14:55:19 2017
 """
 
 import sys
+import math
 from PyQt4 import QtGui, QtCore
-import random
+from itertools import product as iter_product
+from itertools import cycle as iter_cycle
 
 from results_visualiser_ui import Ui_MainWindow
 from results_loader import ResultsLoader
 
+_debug = True
+
 class Window(QtGui.QMainWindow, Ui_MainWindow):
+    
     def __init__(self):
         QtGui.QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
-        
+
+        self.results = ResultsLoader()
+        self.isReady2Plot = False
+
+        self.ax = self.mpl.canvas.fig.add_subplot(111)
+
         self.listHs.setSelectionMode(QtGui.QAbstractItemView.ExtendedSelection)
         self.listTp.setSelectionMode(QtGui.QAbstractItemView.ExtendedSelection)
         self.listHeading.setSelectionMode(QtGui.QAbstractItemView.ExtendedSelection)
         
         # connect the signals with the slots
+        self.comboBox.currentIndexChanged .connect(self.selectionChangedVariable)
         self.actionOpen.triggered.connect(self.open_file)
         self.listHs.itemSelectionChanged.connect(self.selectionChangedHs)
-        
-        self.fill_dummy()
-        self.plot_dummy()
+        self.listTp.itemSelectionChanged.connect(self.check_state)
+        self.listHeading.itemSelectionChanged.connect(self.check_state)
+        self.radio_log.toggled.connect(self.check_state)
+        self.radio_linear.toggled.connect(self.check_state)
+        self.radio_min.toggled.connect(self.check_state)
+        self.radio_max.toggled.connect(self.check_state)
+        self.checkBoxLegend.toggled.connect(self.check_state)
+
+    def resizeEvent(self, event):
+        if _debug: print('resize event called - w=', self.width(), 'h=', self.height())
+        self.adjust_n_draw_canvas()
 
     def open_file(self):
+        if _debug: print('open_file called')
         fname = QtGui.QFileDialog.getOpenFileName(self, 'OpenFile')
-        self.results = ResultsLoader(fname)
-        self.comboBox.clear()
-        self.comboBox.addItems(self.results.get_vars())
+        self.results.load(fname)
+        if _debug: print('clearing lists')
         self.listHs.clear()
-        self.listHs.addItems(self.results.get_hs_list())
-        self.listHeading.clear()
-        self.listHeading.addItems(self.results.get_wd_list())
         self.listTp.clear()
+        self.listHeading.clear()
+        self.comboBox.clear()
+        if _debug: print('filling lists')
+        self.listHs.addItems(self.results.get_hs_list())
+        self.listHs.setCurrentRow(0)
+        self.listHeading.addItems(self.results.get_wd_list())
+        self.listHeading.setCurrentRow(0)
+        self.comboBox.addItems(self.results.get_vars())
+
+    def check_state(self):
+        if _debug: print('check_state called')
+        self.isReady2Plot = False
+        if ((len(list(self.listHs.selectedItems())) > 0) &
+            (len(list(self.listTp.selectedItems())) > 0) &
+            (len(list(self.listHeading.selectedItems())) > 0) &
+            (self.comboBox.count() > 0)):
+            self.isReady2Plot = True
+            if _debug: print('   state ready2plot')
+            self.plot()
+        else:
+            if _debug: print('   state not ready2plot')
 
     def selectionChangedHs(self):
+        if _debug: print('selectionChangedHs called')
         self.listTp.clear()
-        print(self.listHs.selectedItems())
-        print(self.results.get_tp_list(self.listHs.selectedItems()))
-        self.listTp.addItems(self.results.get_tp_list(self.listHs.selectedItems()))
-       
-    def fill_dummy(self):
-        self.comboBox.addItems(["var1", "var2", "var3 max", "var4 min"])
-        
-        self.listHs.addItems(['1', '2', '3.5'])
-    
-    def plot_dummy(self):
-        ''' plot some random stuff '''
+        self.listTp.addItems(self.results.get_tp_list(
+                             [x.text() for x in self.listHs.selectedItems()]))
+        self.listTp.setCurrentRow(0)
+        self.check_state()
 
-        # create an axis
-        ax = self.mpl.canvas.fig.add_subplot(111)
+    def selectionChangedVariable(self):
+        if _debug: print('selectionChangedVariable called')
+        var = self.comboBox.currentText()
+        if 'max' in var.lower():
+            self.radio_max.setChecked(True)
+        elif 'min' in var.lower():
+            self.radio_min.setChecked(True)
+        self.check_state()
 
-        # discards the old graph
-        ax.clear()
+    def plot(self):
+        if _debug: print('plot called')
+        if not (self.isReady2Plot or self.results.isAvailable):
+            if _debug: print('plot returned not ready')
+            return
+        self.ax.clear()
+        var = self.comboBox.currentText()
+        y = list(self.results.llcdf if self.radio_log.isChecked() else self.results.cdf)
+        if self.radio_min.isChecked():
+            y.reverse()
+        # shapes and colors for plot
+        marker = plot_marker_style()
+        hasData = False
+        for hs, tp, wd in iter_product(
+                          [float(x.text()) for x in self.listHs.selectedItems()],
+                          [float(x.text()) for x in self.listTp.selectedItems()],
+                          [float(x.text()) for x in self.listHeading.selectedItems()]):
+            sample = self.results.get_sample(var, hs, tp, wd)
+            if not sample.empty:
+                hasData = True
+                self.ax.plot(sample, y, next(marker), label='Hs{} Tp{} wd{}'.format(hs, tp, wd))
+        if hasData:
+            self.adjust_n_draw_canvas()
 
-        # plot data
-        ax.plot(*self.load_res_dummy(165), 'o')
-        ax.plot(*self.load_res_dummy(180), 'o')
-        ax.plot(*self.load_res_dummy(195), 'o')
+    def adjust_n_draw_canvas(self):
+            self.ax.get_yaxis().grid(True)
+            self.ax.get_xaxis().grid(True)
+            numitems = len(list(self.ax._get_legend_handles()))
+            pad_top = 1
+            if numitems and self.checkBoxLegend.isChecked(): 
+                # trying to adjust the size of the legend and the plot to something reasonable
+                ncols = math.ceil(self.mpl.canvas.width()/150)  # 150 px assumed size of one label
+                nrows = math.ceil(numitems/ncols)
+                pad_top = max(0.6, 1.0 - 0.025 * nrows)  # pad at top to leave room for legend
+                #print('ncols', ncols, 'nrows', nrows, 'pad top', pad_top)
+                self.ax.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, borderaxespad=0,
+                               mode='expand', ncol=ncols, prop={'size': 8})
+            self.mpl.canvas.fig.tight_layout(rect=(0, 0, 1, pad_top))  # left, bottom, right, top
+            self.mpl.canvas.draw()
 
-        # refresh canvas
-        self.mpl.canvas.fig.tight_layout()
-        self.mpl.canvas.draw()
-    
     def load_res_dummy(self, wd):
         import pandas as pd
         df = pd.read_table("Results.txt", sep='\t')
@@ -76,6 +141,15 @@ class Window(QtGui.QMainWindow, Ui_MainWindow):
                     (df['WaveDirection'] == wd)]['Link1 Max Tension']
         return sample.sort_values(), range(len(sample))
 
+def plot_marker_style():
+        # shapes and colors for plot
+        colors = 'bgrcmyk'
+        shapes = 'ov^<>12348sp*hH+xDd|_'
+        marker = iter_cycle(iter_product(shapes, colors))
+        while True:
+            cur = '%c%c' % next(marker)
+            if _debug: print('generator marker: ', cur)
+            yield cur
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
