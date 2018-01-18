7c67cfa35ae813c8f7e4ee8508953caa8984bfa1
Rafael Rossi <rossirafael@yahoo.com>
2018-01-18 15:12:47 +0100

    added an offset functions to catenarylib

--- Qt/Catenary/catenarylib.py
+++ Qt/Catenary/catenarylib.py
@@ -159,6 +159,74 @@ def CatenarySolveV_HLMBR(V, param, flag='H'):
     return CatenaryCalcTAV(TAi, V)
 
 
+def CatenaryOffset(params, DeltaH=0, DeltaV=0):
+    """CatenaryOffset(params, DeltaH, DeltaV) -> TA, V, H, L, MBR
+
+    Apply an offset to the top of the catenary.
+    """
+
+    TA, V, H, L, MBR = params
+
+    # Check if input values are reasonable
+    UpperBoundDeltaH = 0.2 * V
+    LowerBoundDeltaH = -min(0.2 * V, H / 2)
+    UpperBoundDeltaV = min(50, V / 10)
+    LowerBoundDeltaV = -UpperBoundDeltaV
+    UpperBoundTA = 75
+    LowerBoundTA = 2
+
+    if DeltaH > UpperBoundDeltaH:
+        print("Maximum offset restraint to 20% of the depth.")
+        return
+    elif DeltaH < LowerBoundDeltaH:
+        print("Minimum offset restraint to 20% of the depth or 50% of the horizontal length.")
+        return
+    elif DeltaV > UpperBoundDeltaV or DeltaV < LowerBoundDeltaV:
+        print("Vertical excursions restraint to either 50m or 10% of depth, whichever is smaller.")
+        return
+    elif TA > UpperBoundTA and (DeltaH > 0 or DeltaV > 0):
+        print("Far offsets and upwards excursions not allowed for top angles > 75deg")
+        return
+    elif TA < LowerBoundTA and (DeltaH < 0 or DeltaV < 0):
+        print("Near offsets and downwards excursions not allowed for top angles < 2deg")
+        return
+
+    # Data for "offset" SCR
+    Vo = V + DeltaV
+
+    # Newton solver to find root of functions
+
+    delta = 0.1  # delta TA to estimate derivative
+    tolerance = 0.01
+    f_TA = 999
+    i = 0
+    TAo = TA  # initial guess for TA
+    while abs(f_TA) > tolerance:
+        i += 1
+        # Calculate f(TAo-delta)
+        retval = CatenaryCalcTAV(TAo - delta, Vo)
+        Ho = retval[2]
+        Lo = retval[3]
+        f_TA_menos_delta = L - Lo - H + Ho - DeltaH
+        # calculate f(TAo+delta)
+        retval = CatenaryCalcTAV(TAo + delta, Vo)
+        Ho = retval[2]
+        Lo = retval[3]
+        f_TA_mais_delta = L - Lo - H + Ho - DeltaH
+        # calculare derivative
+        f_linha = (f_TA_mais_delta - f_TA_menos_delta) / delta
+        # calculate f(TAo)
+        retval = CatenaryCalcTAV(TAo, Vo)
+        Ho = retval[2]
+        Lo = retval[3]
+        f_TA = L - Lo - H + Ho - DeltaH
+        # Newton-Raphson Iteration
+        TAo = TAo - f_TA / f_linha
+
+    MBRo = retval[4]
+    return TAo, Vo, Ho, Lo, MBRo
+
+
 def unit_tests():
     """Test all catenary functions for a known solution."""
     from numpy import allclose
@@ -172,7 +240,9 @@ def unit_tests():
            allclose((TA, V, H, L, MBR), CatenarySolveH_LMBR(H, MBR, flag='MBR')),
            allclose((TA, V, H, L, MBR), CatenarySolveV_HLMBR(V, H, flag='H')),
            allclose((TA, V, H, L, MBR), CatenarySolveV_HLMBR(V, L, flag='L')),
-           allclose((TA, V, H, L, MBR), CatenarySolveV_HLMBR(V, MBR, flag='MBR')))):
+           allclose((TA, V, H, L, MBR), CatenarySolveV_HLMBR(V, MBR, flag='MBR')),
+           allclose((TA, V, H, L, MBR),
+                    CatenaryOffset(CatenaryOffset(CatenaryCalcTAV(TA, V), -5, 5), 5, -5)))):
         return True
     else:
         return False
