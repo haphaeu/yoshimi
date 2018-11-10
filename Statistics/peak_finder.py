7e51632cd19c10febdcf18618705a0857faa2323
Rafael Rossi <rossirafael@yahoo.com>
2018-11-10 19:52:09 +0100

    statistics, weibull and peak finding, notebook

---
+++ Statistics/peak_finder.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+"""
+
+[1] DNV GL JIP Practical Guidance to Estimation of Response Extremes
+    Report No.: 2015-0684, Rev. 0
+    Document No.: 186RZ20-49
+    Date: 2015-09-07
+
+[2] Theory and derivation for Weibull parameter probability weighted moment estimators
+    https://books.google.no/books?id=d6OJf5SuP9YC
+
+Created on Fri Nov  9 21:11:20 2018
+
+@author: rarossi
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import OrcFxAPI as of
+import scipy.stats as ss
+from numpy import log
+from scipy.special import binom
+from scipy.special import gamma as gamma_function
+
+
+def M(j, x):
+    '''Probability Weighted Moments.
+
+    x must be sorted.
+
+    See [1] and eq. (32) in [2]
+    '''
+    n = len(x)
+    return sum([x[i-1] * binom(i-1, j)/binom(n-1, j)
+                for i in range(j+1, n)])/n
+
+
+def weibull_fit(x):
+    '''Fit Weibull parameters using PWM.
+
+    Return (shape, location, scale).
+
+    See section 2.2.3.1 of [1] and [2].
+    '''
+    M0 = M(0, x)
+    M1 = M(1, x)
+    M2 = M(2, x)
+    M3 = M(3, x)
+
+    delta = (4 * (M0 * (3 * M2 - M3 - M1) - M1**2) /
+             (M0 - 8*M1 + 12*M2 - 4*M3))
+
+    gamma = log(2) / log((2*M1 - M0) /
+                         (2 * (5*M1 - M0 - 6*M2 + 2*M3)))
+
+    beta = (M0 - delta) / gamma_function(1 + 1/gamma)
+
+    return gamma, delta, beta
+
+
+def weib2gumb(params, N):
+    '''Explicity calculation of the parameters of a Gumbel distribution
+    of extreme response for a Weibull distribution of response peaks.
+
+    Returns
+    -------
+    loc, scale:
+        Parameters of Gumbel distribution.
+
+    Arguments
+    ---------
+    params:
+        Weibull distribution parameters (shape, loc, scale).
+
+    N:
+        Size of the sample
+
+    See [1].
+    '''
+    g, d, b = params
+    return (d + b * log(N)**(1/g), b/g * log(N)**(1/g-1))
+
+
+def global_peaks(values, times):
+    '''Detects global peaks of a signal.
+
+    Returns the global peaks and respective times.
+    '''
+
+    # brute forcing a litte
+
+    # shift by average to make sure there are zero crossings
+    ref = sum(values)/len(values)
+
+    # zero up-crossings
+    zuc = [i for i in range(len(values)-1) if values[i] < ref and values[i+1] > ref]
+
+    # indexes of the peaks
+    index = [zuc[i] + values[zuc[i]:1+zuc[i+1]].argmax() for i in range(len(zuc)-1)]
+
+    return values[index], times[index]
+
+
+def fetch_timetrace(sim, list_vars):
+
+    # very simplified fetch of timetraces
+
+    print('Loading', sim, end='.')
+    m = of.Model(sim)
+    print(' Done.')
+    ttraces = dict()
+    ttraces['time'] = m.general.TimeHistory('Time', 1)
+
+    for var in list_vars:
+        name = ' '.join(var)
+        if var[1].lower() == 'enda':
+            oe = of.oeEndA
+        elif var[1].lower() == 'endb':
+            oe = of.oeEndB
+        elif var[1].lower() in ('touchdown', 'tdp', 'tdz'):
+            oe = of.oeArcLength(m['Line1'].StaticResult('Arc Length', of.oeTouchdown))
+        else:
+            raise SystemError
+        ttraces[name] = m['Line1'].TimeHistory(var[0], 1, objectExtra=oe)
+
+    return ttraces
+
+
+# See jupyter notebook...
+# peak_finder_weibull_fit
--- Statistics/weibull_max_min.py
+++ Statistics/weibull_max_min.py
@@ -2,6 +2,21 @@
 """
 https://stats.stackexchange.com/questions/374973/what-is-the-most-probable-maximum-for-a-weibull-distribution
 
+
+Weibull parameters:
+    shape, k:
+        k > 0
+        distribution only has a mode for k > 1
+
+    location:
+
+    scale, lambda:
+
+The mode (MPM) is:
+    lambda * ((k-1)/k)**(1/k)
+    scale * ((shape-1)/shape)**(1/shape)
+
+
 Created on Fri Nov  2 11:50:54 2018
 
 @author: rarossi
@@ -13,34 +28,43 @@ from numpy import linspace
 
 # Create a sample, right skewed to represent a distribution of maxima
 
-sz = 10000
-sample = stats.gumbel_r.rvs(loc=100, scale=3, size=sz)
-sample -= min(sample)
+sz = 1000
+#sample = stats.gumbel_r.rvs(loc=100, scale=3, size=sz)
+#sample = stats.norm.rvs(loc=100, scale=3, size=sz)
+sample = stats.weibull_min.rvs(c=5, loc=100, scale=3, size=sz)
+#sample -= min(sample)
 x, eps = linspace(min(sample), max(sample), sz, retstep=True)
 
 # Fit Weibull to the sample
 
-# first using weibull_max
-wb_mx = stats.weibull_max
-params_wb_mx = wb_mx.fit(sample)
-d_mx = wb_mx(*params_wb_mx)
+## first using weibull_max
+#wb_mx = stats.weibull_max
+#params_wb_mx = wb_mx.fit(sample)
+#k, loc, lbd = params_wb_mx
+#mode_mx = lbd * (1-1/k)**(1/k) + loc
+#d_mx = wb_mx(*params_wb_mx)
 
 # then using weibull_min
 wb_mn = stats.weibull_min
 params_wb_mn = wb_mn.fit(sample)
+k, loc, lbd = params_wb_mn
+mode_mn = lbd * (1-1/k)**(1/k) + loc
 d_mn = wb_mn(*params_wb_mn)
 
 # Plot everything
 
 pp.figure(num=None, figsize=(9, 4), dpi=80, facecolor='w', edgecolor='k')
-pp.plot(x, d_mx.pdf(x), label='Weibull Max')
-#pp.plot(x, d_mx.cdf(x), label='Weibull Max')
+
+#pp.plot(x, d_mx.pdf(x), label='Weibull Max')
+## pp.plot(x, d_mx.cdf(x), label='Weibull Max')
+
 pp.plot(x, d_mn.pdf(x), label='Weibull Min')
-#pp.plot(x, d_mn.cdf(x), label='Weibull Min')
+# pp.plot(x, d_mn.cdf(x), label='Weibull Min')
 pp.hist(sample, bins=50, normed=True)
 pp.legend(loc='best')
 pp.grid()
 pp.show()
 
-print('Weibull Max {:12.1f} {:12.1f} {:12.1f}'.format(*params_wb_mx))
-print('Weibull Min {:12.1f} {:12.1f} {:12.1f}'.format(*params_wb_mn))
+print('            {:>12s} {:>12s} {:>12s} {:>12s} {:>12s}'.format('Shape', 'Loc', 'Scale', 'Mode', 'cdf(Mode)'))
+#print('Weibull Max {:12.1f} {:12.1f} {:12.1f} {:12.1f} {:12.2f}'.format(*params_wb_mx, mode_mx, d_mx.cdf(mode_mx)))
+print('Weibull Min {:12.1f} {:12.1f} {:12.1f} {:12.1f} {:12.2f}'.format(*params_wb_mn, mode_mn, d_mn.cdf(mode_mn)))
