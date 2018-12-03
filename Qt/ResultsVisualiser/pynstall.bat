392eadefbe1897379a88c963cafd0ef66c9915ec
Rafael Rossi <rossirafael@yahoo.com>
2018-12-03 14:45:54 +0100

    Qt ResultsVisualiser moved to another repo in bitbucket.

--- Qt/ResultsVisualiser/pynstall.bat
+++
@@ -1,6 +0,0 @@
-:: need to install pyinstall
-::  > pip install pyinstaller
-
-pyinstaller --clean --windowed --add-data="help.txt;." --add-data="icon.png;." results_visualiser.py
-
-pause
--- Qt/ResultsVisualiser/ui2py.bat
+++
@@ -1,6 +0,0 @@
-::
-:: This step cannot be made portable PyQt4 and PyQt5
-:: Will have to handle it manually...
-::
-call pyuic4 results_visualiser.ui > tmp_ui.py
-pause
