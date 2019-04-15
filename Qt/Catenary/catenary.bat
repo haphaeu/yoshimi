851bca5ec62fdc380f22d7dcccae7bfda3fd58db
Rafael Rossi <rossirafael@yahoo.com>
2019-04-15 09:46:52 +0200

    updated launcher with with conda env

--- Qt/Catenary/catenary.bat
+++ Qt/Catenary/catenary.bat
@@ -1,2 +1,13 @@
+@echo off
+echo Launching application in a conda environment...
+call  C:\Anaconda3\Scripts\activate.bat C:\Anaconda3
+call conda activate qt5
+if errorlevel 1 (
+    echo !!!
+    echo !!! Error activating environemt.
+    echo !!! Have you run the installer?
+    echo !!!
+    goto :EOF
+)
 cd /d %~dp0
-python catenary.py
+python catenary_qt5.py
