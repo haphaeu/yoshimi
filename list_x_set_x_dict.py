eb650dbfefd4582f9233b1d77dd77bbd7efe3925
Rafael Rossi <rossirafael@yahoo.com>
2019-10-04 09:26:40 +0200

    Converted list set and dict speed test to py3

--- list_x_set_x_dict.py
+++ list_x_set_x_dict.py
@@ -7,52 +7,69 @@ from time import time
 L=[randint(100000,999999) for _ in range(100000)]
 
 #create a set from L
-S=set(L)
+S = set(L)
 
 #create a dict from L
 # (is there a better way to do this?)
-D={}
-for _ in L:
-    D[_]=_
+D = {}
+for i in L:
+    D[i] = i
 
-START=123456
-END  =START+10000
+start = 123456
+end  = start + 10000
+
 # ##### RUN ##### ########################################
 # now performs random searches and add elements in each 
 # group to check for speed
-print "List x Set x Dict"
-print "Performing %d random searches" % (END-START)
-print "Here we go..."
-#SET
-print "Set",
-i=START
-st=time()
-while i<END:
-    if i in S: pass
-    else: S.add(i)
-    i+=1
-ts=time()-st
-print "- took %.4fs" % ts
+
+print("List x Set x Dict")
+print("Performing %d random searches" % (end - start))
+print("Here we go...")
+
+# SET
+print("Set", end='')
+i = start
+st = time()
+
+while i < end:
+    if i in S: 
+        pass
+    else: 
+        S.add(i)
+    i += 1
+
+ts = time() - st
+print("- took %.4fs" % ts)
+
 # DICT
-print "Dict",
-i=START
-st=time()
-while i<END:
-    if i in D: pass
-    else: D[i]=i
-    i+=1
-td=time()-st
-print "- took %.4fs" % td
+print("Dict", end='')
+i = start
+st = time()
+
+while i < end:
+    if i in D: 
+        pass
+    else: 
+        D[i]=i
+    i += 1
+
+td = time() - st
+print("- took %.4fs" % td)
+
 # LIST
-print "List",
-i=START
-st=time()
-while i<END:
-    if i in L: pass
-    else: L.append(i)
-    i+=1
-tl=time()-st
-print "- took %.4fs" % tl
+print("List", end='')
+i = start
+st = time()
+
+while i < end:
+    if i in L: 
+        pass
+    else: 
+        L.append(i)
+    i += 1
+
+tl = time() - st
+print("- took %.4fs" % tl)
 
 '''
 OUTPUT
